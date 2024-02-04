2621cb055642498990e5fb347f29931dd1f46174
tylerelk <144300250+tylerelk@users.noreply.github.com>
2024-02-04 15:32:58 -0800

    complete mobile version

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 const expandArrow = document.querySelector('.expand-icon');
-const postArea = document.querySelector('.entry-field');
+const postArea = document.querySelector('nav');
 
 expandArrow.addEventListener('click', () => {
     if (postArea.style.transform === 'scaleY(0)'){
