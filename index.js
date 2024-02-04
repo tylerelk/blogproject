93d83c33724e6a30e20712b86da742b5818ea9ee
tylerelk <144300250+tylerelk@users.noreply.github.com>
2024-02-03 19:07:56 -0800

    complete post/select logic

--- index.js
+++ index.js
@@ -5,34 +5,45 @@ const app = express();
 const port = 3000;
 
 const allPosts = [];
+const referencePost = (post, arr) => {
+  let thisPost = arr[post.postID];
+  return thisPost.title;
+};
+const data = {
+  posts: allPosts,
+  reference: referencePost,
+};
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { data: data });
 });
 
-app.post('/submit', (req, res) => {
-    let post = new BlogPost(req.body.title, req.body.author, req.body.content);
-    allPosts.push(post.postID);
-    console.log(allPosts);
-    res.render('index.ejs', {title: post.title, author: post.author, content: post.content});
-})
+app.post("/submit", (req, res) => {
+  let post = new BlogPost(req.body.title, req.body.author, req.body.content);
+  allPosts.push(post);
+  res.render("index.ejs", { data: data, postID: post.postID });
+});
+
+app.post("/select", (req, res) => {
+  res.render("index.ejs", { data: data, postID: Number(req.body.postSelection) });
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
 class BlogPost {
-    constructor(title, author, content) {
-        this.title = title;
-        this.author = author;
-        this.content = content;
-        this.postID = this.generateID(this.title);
-    };
+  constructor(title, author, content) {
+    this.title = title;
+    this.author = author;
+    this.content = content;
+    this.postID = this.generateID();
+  }
 
-    generateID(title) {
-     return `post-${title}`;
-    }
+  generateID() {
+    return allPosts.length;
+  }
 }
